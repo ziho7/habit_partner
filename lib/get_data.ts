9e2ebb44680cd41ab694b022a6a6cef623311760
ziho7 <78910421@qq.com>
2024-06-13 06:32:39 +0800

    feat add getCurrentDate function

--- lib/get_data.ts
+++ lib/get_data.ts
@@ -1,11 +1,32 @@
 export const getDayData = function getDayData() {
-  return [
-    {
-      name: 'Sport',
-      startDate: '2024.03.07',
-      endDate: '2024.09.07',
-      frequency: '每天一次',
-      days: '30'
-    }
-  ];
+    return [
+        {
+            name: 'Sport',
+            startDate: '2024-03-07',
+            endDate: '2024-09-07',
+            frequency: '每天一次',
+            days: '30'
+        }
+    ];
 }
+
+const getOneData = function getOneData(id: number) {
+    return
+}
+
+export const getCurrentDateAndDayOfWeekInTimeZone = (timeZone: string) => {
+    const now = new Date();
+    const formatter = new Intl.DateTimeFormat('en-US', {
+        weekday: 'long', // "Monday", "Tuesday", etc.
+        year: 'numeric',
+        month: '2-digit', // "01", "02", etc.
+        day: '2-digit', // "01", "02", etc.
+        timeZone: timeZone, // Specify the timezone
+    });
+
+    const formattedDate = formatter.format(now).split(', ');
+    const dayOfWeek = formattedDate[0]; // The day of the week
+    const currentDate = `${formattedDate[3]}-${formattedDate[1]}-${formattedDate[2]}`; // Format: YYYY-MM-DD
+
+    return { currentDate, dayOfWeek };
+};
