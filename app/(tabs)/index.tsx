60419a3baa297ae754cb23d99663b281fa48ea6b
ziho7 <78910421@qq.com>
2024-06-13 16:26:15 +0800

    feat add second card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,14 +5,67 @@ import CustomButton from '@/components/CustomButton'
 // import {images} from '../../constants'
 import images from '@/constants/images'
 import Donut2 from '@/components/Donut2'
+import { useGlobalContext } from '@/context/GlobalProvider'
+import { getCurrentDateAndDayOfWeekInTimeZone, getDayDataByUserId } from '@/lib/get_data'
+
+
+const HabitCard = ({
+  beginDate,
+  endDate,
+  totalCount,
+  name,
+}: {
+  beginDate: string,
+  endDate: string,
+  totalCount: string,
+  name: string
+}) => {
+  return <View className='mt-4'>
+        <View className='flex-row justify-between items-center h-[160px] bg-mypurple-light rounded-xl px-4'>
+          <View className='flex-col'>
+            <View className='flex-row items-center space-x-4 mb-2'>
+              <View className='bg-mypurple h-[36px] w-[36px] rounded-full justify-center items-center'>
+                <Image
+                  source={images.ball}
+                  className='w-[26px] h-[26px]'
+                  resizeMode='contain'
+                />
+              </View>
+              <Text className='font-semibold text-[20px]'>{name}</Text>
+            </View>
+            <Text className='text-[12px] text-mygray'>{beginDate}-{endDate}</Text>
+            {/* <Text className='text-[12px] text-mygray'>每天一次</Text> */}
+            <Text className='text-[12px] text-mygray'>Completed: {totalCount}</Text>
+          </View>
+
+          {/* <Donut /> */}
+          
+          <Donut2 />
+
+
+        </View>
+      </View>
+}
 
 const Home = () => {
+  const { loading, isLogged } = useGlobalContext()
+
+  const {currentDate, dayOfWeek} = getCurrentDateAndDayOfWeekInTimeZone()
+
+  const dayHabit = getDayDataByUserId(123)
+
+  
+  
+
   return (
     <SafeAreaView className='mx-4'>
       {/* title */}
       <View className='flex-row h-16 justify-between items-center'>
         <Text className='font-extrabold text-[24px]'>Habit Partner</Text>
-        <Text className=''>June 11</Text>
+        <View>
+        <Text className=''>{currentDate}</Text>
+        <Text className=''>{dayOfWeek}</Text>
+        </View>
       </View>
       {/* calender */}
       <View className='flex-row h-16 justify-between items-center'>
@@ -30,31 +83,8 @@ const Home = () => {
       </View>
 
       {/* habit list */}
-      <View className='mt-4'>
-        <View className='flex-row justify-between items-center h-[160px] bg-mypurple-light rounded-xl px-4'>
-          <View className='flex-col'>
-            <View className='flex-row items-center space-x-4 mb-2'>
-              <View className='bg-mypurple h-[36px] w-[36px] rounded-full justify-center items-center'>
-                <Image
-                  source={images.ball}
-                  className='w-[26px] h-[26px]'
-                  resizeMode='contain'
-                />
-              </View>
-              <Text className='font-semibold text-[20px]'>Sport</Text>
-            </View>
-            <Text className='text-[12px] text-mygray'>2024.03.07-2024.09.07</Text>
-            <Text className='text-[12px] text-mygray'>每天一次</Text>
-            <Text className='text-[12px] text-mygray'>已坚持30天</Text>
-          </View>
-
-          {/* <Donut /> */}
-          
-          <Donut2 />
-
-
-        </View>
-      </View>
+      <HabitCard beginDate='2024-06-12' endDate='2024-09-12' totalCount='1' name='Sport'/>
+      <HabitCard beginDate='2024-06-11' endDate='2024-09-12' totalCount='1' name='Work'/>
 
     </SafeAreaView>
   )
--- lib/get_data.ts
+++ lib/get_data.ts
@@ -1,32 +1,64 @@
-export const getDayData = function getDayData() {
-    return [
-        {
-            name: 'Sport',
-            startDate: '2024-03-07',
-            endDate: '2024-09-07',
-            frequency: '每天一次',
-            days: '30'
-        }
-    ];
+import { getCalendars } from 'expo-localization';
+
+const database = [
+    {
+        id: 1,
+        user_id: "123",
+        name: 'Sport',
+        days: '30',
+        startDate: '2024-03-07',
+        endDate: '2024-09-07',
+        creatorId: '1231239',
+        everycount: '10',
+        type: 0,
+        showsDays: [1, 2, 3]
+    },
+    {
+        id: 2,
+        user_id: "123",
+        name: 'Sport',
+        days: '30',
+        startDate: '2024-03-07',
+        endDate: '2024-09-07',
+        creatorId: '1231239',
+        everycount: '10',
+        type: 0,
+        showsDays: [1, 2, 3]
+    },
+    {
+        id: 3,
+        user_id: "123",
+        name: 'Sport',
+        days: '30',
+        startDate: '2024-03-07',
+        endDate: '2024-09-07',
+        creatorId: '1231239',
+        everycount: '10',
+        type: 0,
+        showsDays: [1, 2, 3]
+    },
+]
+
+export const getDayDataByUserId = function getDayData(user_id: number) {
+    return database
 }
 
-const getOneData = function getOneData(id: number) {
-    return
-}
 
-export const getCurrentDateAndDayOfWeekInTimeZone = (timeZone: string) => {
+// todo 传值
+export const getCurrentDateAndDayOfWeekInTimeZone = () => {
+    const { timeZone } = getCalendars()[0];
     const now = new Date();
     const formatter = new Intl.DateTimeFormat('en-US', {
         weekday: 'long', // "Monday", "Tuesday", etc.
         year: 'numeric',
         month: '2-digit', // "01", "02", etc.
         day: '2-digit', // "01", "02", etc.
-        timeZone: timeZone, // Specify the timezone
+        timeZone: timeZone || "", // Specify the timezone
     });
 
     const formattedDate = formatter.format(now).split(', ');
     const dayOfWeek = formattedDate[0]; // The day of the week
-    const currentDate = `${formattedDate[3]}-${formattedDate[1]}-${formattedDate[2]}`; // Format: YYYY-MM-DD
+    const currentDate = `${formattedDate[1]}`;
 
     return { currentDate, dayOfWeek };
 };
