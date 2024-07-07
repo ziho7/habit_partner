c22544a038f71485396f65302bd494c78ffdafc7
ziho7 <78910421@qq.com>
2024-07-07 10:17:10 +0800

    feat add slected days

--- components/ShowDaysModal.tsx
+++ components/ShowDaysModal.tsx
@@ -11,8 +11,18 @@ const ShowDaysModal = ({ showPicker, closeFunction, onChangeFunction, pickData }
     const getColor = (dayNumber: number) => {
         if (pickData.includes(dayNumber)) {
             return 'bg-[#CEBEE8]'
-        } 
+        }
         return 'bg-[#FFFFFF]'
+    }
+
+    const onPressBlock = (dayNumber: number) => {
+        if (pickData.includes(dayNumber)) {
+            const index = pickData.indexOf(dayNumber)
+            pickData.splice(index, 1)
+        } else {
+            pickData.push(dayNumber)
+        }
+        onChangeFunction(pickData)
     }
 
     return <Modal
@@ -27,7 +37,7 @@ const ShowDaysModal = ({ showPicker, closeFunction, onChangeFunction, pickData }
         <TouchableOpacity
             className='h-1/4 flex-1 justify-end bg-black broder-1 border-black'
             activeOpacity={1.0}
-            // onPressOut={closeFunction}
+        onPressOut={closeFunction}
         >
             <View
                 className=' bg-[#FFFFFF] p-4 rounded-xl my-4 space-y-6 h-[200px]'
@@ -39,37 +49,58 @@ const ShowDaysModal = ({ showPicker, closeFunction, onChangeFunction, pickData }
                 </View>
 
                 <View className='flex-row px-2 justify-center jusitfy-center items-center gap-x-4'>
-                    <TouchableOpacity className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(0)}`}>
+                    <TouchableOpacity
+                        className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(0)}`}
+                        onPress={() => onPressBlock(0)}
+                    >
                         <Text className='text-[12px] font-light'>
                             Sun
                         </Text>
                     </TouchableOpacity>
-                    <TouchableOpacity className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(1)}`}>
+                    <TouchableOpacity
+                        className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(1)}`}
+                        onPress={() => onPressBlock(1)}
+                    >
                         <Text className='text-[12px] font-light'>
                             Mon
                         </Text>
                     </TouchableOpacity>
-                    <TouchableOpacity className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(2)}`}>
+                    <TouchableOpacity
+                        className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(2)}`}
+                        onPress={() => onPressBlock(2)}
+                    >
                         <Text className='text-[12px] font-light'>
                             Tue
                         </Text>
                     </TouchableOpacity>
-                    <TouchableOpacity className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(3)}`}>
+                    <TouchableOpacity
+                        className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(3)}`}
+                        onPress={() => onPressBlock(3)}
+                    >
                         <Text className='text-[12px] font-light'>
                             Wed
                         </Text>
                     </TouchableOpacity>
-                    <TouchableOpacity className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(4)}`}>
+                    <TouchableOpacity
+                        className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(4)}`}
+                        onPress={() => onPressBlock(4)}
+                    >
                         <Text className='text-[12px] font-light'>
                             Thu
                         </Text>
                     </TouchableOpacity>
-                    <TouchableOpacity className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(5)}`}>
+                    <TouchableOpacity
+                        className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(5)}`}
+                        onPress={() => onPressBlock(5)}
+                    >
                         <Text className='text-[12px] font-light'>
                             Fri
                         </Text>
                     </TouchableOpacity>
-                    <TouchableOpacity className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(6)}`}>
+                    <TouchableOpacity
+                        className={`border border-[#CEBEE8] h-[30px] w-[30px] items-center justify-center rounded-sm ${getColor(6)}`}
+                        onPress={() => onPressBlock(6)}
+                    >
                         <Text className='text-[12px] font-light'>
                             Sat
                         </Text>
