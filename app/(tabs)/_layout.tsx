51517a4d2b9f990bf8f7a0a538263032960d8542
ziho7 <78910421@qq.com>
2024-07-18 17:45:35 +0800

    feat 修改颜色

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,37 +8,41 @@ import { useTranslation } from 'react-i18next';
 
 export default function TabLayout() {
 
-  const {t} = useTranslation()
+  const { t } = useTranslation()
 
   return (
-    
-      <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }} >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: t('home'),
-            headerShown: false,
-            tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
-          }}
-        />
-        <Tabs.Screen
-          name="data"
-          options={{
-            title: 'Data',
-            headerShown: false,
-            href: null,
-            tabBarIcon: ({ color }) => <AntDesign name="barschart" size={28} color={color} />,
-          }}
-        />
-        <Tabs.Screen
-          name="settings"
-          options={{
-            title: t('settings'),
-            headerShown: false,
-            tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
-          }}
-        />
-      </Tabs>
-    
+
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: 'black',
+      tabBarInactiveTintColor: 'gray',
+    }} >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: t('home'),
+          headerShown: false,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          // tabBarActiveTintColor='#CEBEE8'
+        }}
+      />
+      <Tabs.Screen
+        name="data"
+        options={{
+          title: 'Data',
+          headerShown: false,
+          href: null,
+          tabBarIcon: ({ color }) => <AntDesign name="barschart" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: t('settings'),
+          headerShown: false,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
+
   );
 }
---
+++ components/CalendarDescription.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CalendarDescription = () => {
+  return (
+    <View className='flex-row'>
+        <Text className='text-[12px] text-mypurple-light'>Calendar Description</Text>
+    </View>
+  )
+}
+
+export default CalendarDescription
